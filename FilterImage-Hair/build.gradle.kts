b47a8bed6f257c9ba0e50baefc6a477f197b3755
Hoppen2020 <server@wesipull.com>
2025-07-25 09:19:51 +0800

    modify build.gradle.kts

--- FilterImage-Hair/build.gradle.kts
+++ FilterImage-Hair/build.gradle.kts
@@ -1,7 +1,37 @@
+import kotlin.text.ifEmpty
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
+
+val GROUP_ID = "com.github.Hoppen2020"
+val ARTIFACT_ID = "FilterImage-Hair"
+val VERSION = latestGitTag().ifEmpty { "1.0.0-SNAPSHOT" }
+
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return  process.inputStream.bufferedReader().use {bufferedReader ->
+        bufferedReader.readText().trim()
+    }
+}
+
+publishing { // 发布配置
+    publications { // 发布的内容
+        register<MavenPublication>("release") { // 注册一个名字为 release 的发布内容
+            groupId = GROUP_ID
+            artifactId = ARTIFACT_ID
+            version = VERSION
+
+            afterEvaluate { // 在所有的配置都完成之后执行
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
+
 
 android {
     namespace = "co.hoppen.filterimage_hair"
